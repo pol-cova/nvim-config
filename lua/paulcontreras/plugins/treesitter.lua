6a941d87ca57498d2b7bdfb89d0f9aa908696bae
Paul Contreras <p.contreritas22@gmail.com>
2026-07-01 01:56:27 -0600

    Handle missing Treesitter module gracefully

--- lua/paulcontreras/plugins/treesitter.lua
+++ lua/paulcontreras/plugins/treesitter.lua
@@ -1,58 +1,58 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-      "windwp/nvim-ts-autotag",
-    },
-    config = function()
-      -- import nvim-treesitter plugin
-      local treesitter = require("nvim-treesitter.configs")
-  
-      -- configure treesitter
-      treesitter.setup({ -- enable syntax highlighting
-        highlight = {
-          enable = true,
-        },
-        -- enable indentation
-        indent = { enable = true },
-        -- enable autotagging (w/ nvim-ts-autotag plugin)
-        autotag = {
-          enable = true,
-        },
-        -- ensure these language parsers are installed
-        ensure_installed = {
-          "json",
-          "javascript",
-          "typescript",
-          "yaml",
-          "markdown",
-          "markdown_inline",
-          "svelte",
-          "graphql",
-          "bash",
-          "lua",
-          "vim",
-          "dockerfile",
-          "gitignore",
-          "query",
-          "vimdoc",
-          "c",
-          "cpp",
-          "python",
-          "rust",
-          "java",
-          "go",
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-      })
-    end,
-  }
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local ok, treesitter = pcall(require, "nvim-treesitter.configs")
+		if not ok then
+			vim.notify("nvim-treesitter is unavailable; using built-in syntax highlighting", vim.log.levels.WARN)
+			vim.cmd("syntax enable")
+			return
+		end
+
+		treesitter.setup({
+			highlight = {
+				enable = true,
+			},
+			indent = { enable = true },
+			autotag = {
+				enable = true,
+			},
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"yaml",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"graphql",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"query",
+				"vimdoc",
+				"c",
+				"cpp",
+				"python",
+				"rust",
+				"java",
+				"go",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
+}
